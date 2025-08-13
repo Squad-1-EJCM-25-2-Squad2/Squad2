8d8ada50e3b0941d0119e656a149e797c3b40cc9
Joao-Miguel-S <jmsdealencar@gmail.com>
2025-08-13 20:50:04 -0300

    feature: userController
    implementação da controller do usuário com os métodos CRUD, além da função de login

--- back/server.ts
+++ back/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import configDotenv from './src/config/dotenv';
 // import cors from 'cors';
-// import routes from './src/routes/routes';
+import routes from './src/routes/routes';
+import { configAuth } from "./src/middlewares/authentication";
 
 configDotenv();
+configAuth();
 
 const app = express();
 const port = process.env.PORT;
@@ -11,11 +13,9 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
-//app.use(routes);
+app.use(routes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+
 
 app.listen(port, () => {
   console.log(`${process.env.APP_NAME} app listening at http://localhost:${port}`);
