74ad060fe50e62fbacbb1a5229a1c730dbc56203
Joao-Miguel-S <jmsdealencar@gmail.com>
2025-08-17 17:08:05 -0300

    feature:  implementação do favoriteController
    Foi adicionado a controller do favorite. Vale ressaltar que o método readAll retornar o anuncio de todos os favoritos. É possível também remover todos ou apenas 1 favorito, além de adcionar novos

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import { UserController } from "../controllers/userController";
 import { authenticate } from "../middlewares/authentication";
+import { FavoritesController } from "../controllers/favoritesController";
 
 const router = Router();
 
@@ -10,6 +11,15 @@ router.get("/user/:id",authenticate,UserController.readUser);
 router.put("/user/:id",authenticate,UserController.updateUser);
 router.delete("/user/:id",authenticate,UserController.deleteUser);
 router.post("/login",UserController.login);
+
+//rota de teste do uploader
+
+
+//rotas de teste dos favoritos
+router.post("/favorite",FavoritesController.addFavorite);
+router.get("/favorite/:userId",FavoritesController.readAllFavorites);
+router.delete("/favoriteRemove/:id",FavoritesController.removeFavorite);
+router.delete("/favoriteRemoveAll/:userId",FavoritesController.removeAllFavorites);
 
 
 export default router
