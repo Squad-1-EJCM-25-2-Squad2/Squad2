2b2daf2955bbe6f154b36d06586f5afd5b2bcb9e
Luiz Lira <luizliracas@gmail.com>
2025-08-21 12:16:53 -0300

    fix(front): adicona dois estilos para a Footer e finaliza a pagina MegaSalee

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,5 @@
 
+
 import Header from './pages/Header/Header';
 import Footer from './pages/Footer/Footer';
 import MegaSale from './pages/MegaSale/MegaSale';
@@ -9,18 +10,19 @@ function App() {
   return (
     <div>
       <Header />
-      {isMegaSaleActive ? <MegaSale /> : <p>Outro conteúdo da página</p>}
-
+      {isMegaSaleActive ? <MegaSale /> : <div>Conteúdo da outra página</div>}
       {isMegaSaleActive ? (
-        <Footer
-          title="Don't Miss Future Sales!"
-          text="Subscribe to our newsletter and be the first to know about exclusive sales and special offers."
+        <Footer 
+          type="promotional" // Agora o 'type' é válido
+          title="Don't Miss Future Sales!" 
+          text="Subscribe to our newsletter and be the first to know about exclusive sales and special offers." 
           backgroundColor="roxo"
         />
       ) : (
-        <Footer
-          title="Stay in Style"
-          text="Subscribe to our newsletter and be the first to know about new arrivals, exclusive offers, and style tips."
+        <Footer 
+          type="default" // Agora o 'type' é válido
+          title="Stay in Style" 
+          text="Subscribe to our newsletter and be the first to know about new arrivals, exclusive offers, and style tips." 
           backgroundColor="preto"
         />
       )}
