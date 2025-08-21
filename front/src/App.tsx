e74d15d554c92cc8850058002f0a69117af0dcd1
PedroGois <pedro0107.gois@gmail.com>
2025-08-20 21:12:10 -0300

    Adiciona página Wishlist com cards e type.ts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,12 @@
-import Home from "./pages/Home/Home.tsx";
+//import Home from "./pages/Home/Home.tsx";
 import React from "react";
+import WishlistPage from './pages/Wishlist/WishlistPage.tsx';
 
 // O componente App.tsx atua como uma rota para todas as páginas.
 // Neste caso, ele renderiza apenas a Home.
 const App: React.FC = () => {
     return (
-        <Home />
+        <WishlistPage/>
     );
 }
 
